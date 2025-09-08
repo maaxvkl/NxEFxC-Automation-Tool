990be80e6d9836989111af672ea7f35a562eadd3
Maximilian Vosswinkel <max.vosswinkel@gmx.de>
2025-09-08 09:20:48 +0200

    deleted not needed List variables in ReadExcelFile class

--- backend/src/main/java/automation/filehandling/ReadExcelFile.java
+++ backend/src/main/java/automation/filehandling/ReadExcelFile.java
@@ -2,9 +2,6 @@ package automation.filehandling;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,9 +14,6 @@ public class ReadExcelFile {
 
 	private final int NET_POINT_TYPE = 14;
 	private final int POINTS = 3;
-	private List<Row> binaryInputs = new ArrayList<>();
-	private List<Row> analogInputs = new ArrayList<>();
-	private List<Row> binaryOutputs = new ArrayList<>();
 
 	public XSSFWorkbook getWorkbookFromExcelFile(MultipartFile file) throws IOException, IllegalArgumentException {
 		InputStream inputStream = file.getInputStream();
