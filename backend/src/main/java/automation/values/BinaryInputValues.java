78c1786f0e066c8617ccfd0f65b79a39810e48b0
Maximilian Vosswinkel <max.vosswinkel@gmx.de>
2025-10-23 15:56:23 +0200

    -changed some values for all data types

--- backend/src/main/java/automation/values/BinaryInputValues.java
+++ backend/src/main/java/automation/values/BinaryInputValues.java
@@ -7,13 +7,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Component
-
 public class BinaryInputValues {
 
+	
 	private int BIwriteToStringCells[] = { 25, 35, 36, 47, 48, 49, 50, 51, 52, 63, 86, 93, 94, 97, 105, 106, 107, 108,
 			109, 111, 112, 113, 114, 125 };
 	private String BIwriteStringValuestoCell[] = { /* 25 */"Normal", /* 35 */"WAHR", /* 36 */"FALSCH",
-			/* 47 */"ALLGEMEIN", /* 48 */"FALSCH", /* 49 */"FALSCH", /* 50 */"WAHR", /* 51 */"FALSCH", /* 52 */"WAHR",
+			/* 47 */"Allgemein", /* 48 */"FALSCH", /* 49 */"FALSCH", /* 50 */"WAHR", /* 51 */"FALSCH", /* 52 */"WAHR",
 			/* 63 */"Ereignis", /* 86 */"WAHR", /* 93 */"Allgemein", /* 94 */"WAHR", /* 97 */"Inaktiv Aktiv",
 			/* 105 */"Inaktiv", /* 106 */"WAHR", /* 107 */"FALSCH", /* 108 */"FALSCH", /* 109 */"FALSCH",
 			/* 111 */"WAHR", /* 112 */"WAHR", /* 113 */"FALSCH", /* 114 */"WAHR", /* 125 */"Alarm", };
